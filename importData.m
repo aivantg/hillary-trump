206129fe98d961338e07067787d5a0c7c0a5d7ce
fractal1729 <vinjai.vale@gmail.com>
2016-09-30 09:45:03 -0400

    Progress on import

--- importData.m
+++ importData.m
@@ -1,5 +1,14 @@
 function [trump hillary] = importData(filename)
 M = importdata(filename, '\t');
-trump, hillary = '';
-
+trump = '';
+hillary = '';
+for i = 2:length(M)
+    line = strsplit(char(M(i)), '\t');
+    if strcmp(char(line(2)), 'Trump')
+        trump = trump + char(line(3));
+    end
+    if strcmp(char(line(2)), 'Clinton')
+        hillary = hillary + char(line(3));
+    end
+end
 end
