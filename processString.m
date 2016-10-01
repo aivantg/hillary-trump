01c38ee45597ae9bb27d8c828d75a463755415cd
Aivant Goyal <aivantg_developer@yahoo.com>
2016-10-01 19:56:48 -0400

    Finished Process String <Untested>

--- processString.m
+++ processString.m
@@ -1,16 +1,4 @@
-function word_indices = processEmail(email_contents)
-%PROCESSEMAIL preprocesses a the body of an email and
-%returns a list of word_indices
-%   word_indices = PROCESSEMAIL(email_contents) preprocesses
-%   the body of an email and returns a list of indices of the
-%   words contained in the email.
-%
-
-% Load Vocabulary
-vocabList = getVocabList();
-
-% Init return value
-word_indices = [];
+function wordContainer = processTrumpHillaryStrings(trumpString, hillaryString)
 
 % ========================== Preprocess Email ===========================
 
@@ -22,29 +10,26 @@ word_indices = [];
 % email_contents = email_contents(hdrstart(1):end);
 
 % Lower case
-email_contents = lower(email_contents);
+trumpString = lower(trumpString);
+hillaryString = lower(hillaryString)
 
 % Strip all HTML
 % Looks for any expression that starts with < and ends with > and replace
 % and does not have any < or > in the tag it with a space
-email_contents = regexprep(email_contents, '<[^<>]+>', ' ');
+trumpString = regexprep(trumpString, '<[^<>]+>', ' ');
+hillaryString = regexprep(hillaryString, '<[^<>]+>', ' ');
 
 % Handle Numbers
 % Look for one or more characters between 0-9
-email_contents = regexprep(email_contents, '[0-9]+', 'num');
-
-% Handle URLS
-% Look for strings starting with http:// or https://
-email_contents = regexprep(email_contents, ...
-                           '(http|https)://[^\s]*', 'httpaddr');
-
-% Handle Email Addresses
-% Look for strings with @ in the middle
-email_contents = regexprep(email_contents, '[^\s]+@[^\s]+', 'emailaddr');
+trumpString = regexprep(trumpString, '[0-9]+', 'num');
+hillaryString = regexprep(hillaryString, '[0-9]+', 'num');
 
 % Handle $ sign
-email_contents = regexprep(email_contents, '[$]+', 'dol');
+trumpString = regexprep(trumpString, '[$]+', 'dol');
+hillaryString = regexprep(hillaryString, '[$]+', 'dol');
 
+% Create empty word container
+wordContainer = containers.map([], []);
 
 % ========================== Tokenize Email ===========================
 
@@ -54,12 +39,20 @@ fprintf('\n==== Processed String ====\n\n');
 % Process file
 l = 0;
 
-while ~isempty(email_contents)
+while ~isempty(trumpString) || ~isempty(hillaryString)
 
+    isTrump = ~isempty(trumpString);
+    
     % Tokenize and also get rid of any punctuation
-    [str, email_contents] = ...
-       strtok(email_contents, ...
-              [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+    if isTrump
+        [str, trumpString] = ...
+            strtok(trumpString, ...
+                [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+    else
+        [str, hillaryString] = ...
+            strtok(hillaryString), ...
+                [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+    end;
 
     % Remove any non alphanumeric characters
     str = regexprep(str, '[^a-zA-Z0-9]', '');
@@ -75,44 +68,20 @@ while ~isempty(email_contents)
        continue;
     end
 
-    % Look up the word in the dictionary and add to word_indices if
-    % found
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Fill in this function to add the index of str to
-    %               word_indices if it is in the vocabulary. At this point
-    %               of the code, you have a stemmed word from the email in
-    %               the variable str. You should look up str in the
-    %               vocabulary list (vocabList). If a match exists, you
-    %               should add the index of the word to the word_indices
-    %               vector. Concretely, if str = 'action', then you should
-    %               look up the vocabulary list to find where in vocabList
-    %               'action' appears. For example, if vocabList{18} =
-    %               'action', then, you should add 18 to the word_indices
-    %               vector (e.g., word_indices = [word_indices ; 18]; ).
-    %
-    % Note: vocabList{idx} returns a the word with index idx in the
-    %       vocabulary list.
-    %
-    % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
-    %       str2). It will return 1 only if the two strings are equivalent.
-    %
-
-    for i = 1:length(vocabList)
-        if strcmp(vocabList{i}, str) == 1,
-            word_indices(end + 1) = i;
-        end;
+    % Look up the word in the word container
+    try countVector = wordContainer(str); % Slightly concerned whether this would work.
+    catch countVector = [0, 0];
     end;
 
-    % =============================================================
+    % Update the Count
+    if isTrump
+        countVector = [countVector(1)++, countVector(2)];
+    else
+        countVector = [countVector(1), countVector(2)++];
+    end;
 
-
-    % Print to screen, ensuring that the output lines are not too long
-    if (l + length(str) + 1) > 78
-        fprintf('\n');
-        l = 0;
-    end
-    fprintf('%s ', str);
-    l = l + length(str) + 1;
+    % Update the Container
+    wordContainer(str) = countVector;
 
 end
 
