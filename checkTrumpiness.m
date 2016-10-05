c569b2d7a89a9e4c0820174383a8435c90692a08
Aivant Goyal <aivantg_developer@yahoo.com>
2016-10-05 18:33:37 -0400

    Finished Naive Bayes Implementation

--- checkTrumpiness.m
+++ checkTrumpiness.m
@@ -1,9 +1,28 @@
 function checkTrumpiness(str)
-<<<<<<< HEAD
   words = extractWords(str);
-end;
-=======
+  fprintf("Printing Words and Word Container");
+  disp(words);
   global wordContainer;
   disp(wordContainer(str));
-end
->>>>>>> 520de2841ef0deed17e643e4bf46025d6931d648
+
+  hillaryProbs = [];
+  trumpProbs = [];
+
+  for i = 1:length(words)
+    word = words(i);
+    countVector = wordContainer(word);
+    total = countVector(1) + countVector(2);
+    hillaryProbs(i) = countVector(1)/total;
+    trumpProbs(i) = countVector(2)/total;
+  end
+
+  hillaryness = naiveBayes(hillaryProbs);
+  trumpiness = naiveBayes(trumpProbs);
+
+  fprintf("Trumpiness: %f\n Hillaryness: %f\n\n");
+  if hillaryness > trumpiness
+    fprintf("Hillary probably said this");
+  else
+    fprintf("Trump probably said this");
+  end
+end;
--- naiveBayes.m
+++ naiveBayes.m
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-function p = naiveBayes(prior, likelihoods)
-   
-end
-=======
 function P = naiveBayes(probs)
-P = prod(probs)
+  P = prod(probs)
 end
->>>>>>> 520de2841ef0deed17e643e4bf46025d6931d648
