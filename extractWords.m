a9a91d446aebd0b907b5a8c61de62db1f1eb2738
Aivant Goyal <aivantg_developer@yahoo.com>
2016-10-05 21:53:52 -0400

    Fixed Bug Potentially?

--- extractWords.m
+++ extractWords.m
@@ -2,25 +2,24 @@ function [words] = extractWords(str)
   str = processString(str);
   rawWords = strsplit(str);
   numWords =  0;
-  words = [];
 
   for i = 1:length(rawWords)
 
     % Stem the word
     % (the porterStemmer sometimes has issues, so we use a try catch block)
-    try stemmedStr = porterStemmer(strtrim(char(rawWords(i))));
+    try stemmedStr = porterStemmer(strtrim(rawWords(i)));
     catch stemmedStr = ''; continue;
     end
 
     disp(stemmedStr);
-    
+
     if length(str) < 1
          continue;
     end
 
     % Remove any non alphanumeric characters
     numWords = numWords + 1;
-    words(numWords) = char(regexprep(stemmedStr, '[^a-zA-Z0-9]', ''));
+    words(numWords) = regexprep(stemmedStr, '[^a-zA-Z0-9]', '');
   end
   fprintf('Words: ');
   disp(words);
