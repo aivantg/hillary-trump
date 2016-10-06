ec8ff5c920a1207eb91c6f6656b5364c4aa8f67b
Aivant Goyal <aivantg_developer@yahoo.com>
2016-10-05 22:10:41 -0400

    fixed porter stemming

--- extractWords.m
+++ extractWords.m
@@ -1,13 +1,14 @@
 function [words] = extractWords(str)
   str = processString(str);
-  rawWords = strsplit(str);
   numWords =  0;
+  while ~isempty(str)
 
-  for i = 1:length(rawWords)
-
+    % get the first word and set str to what remains
+    [rawStr str] = strtok(str, ...
+                [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
     % Stem the word
     % (the porterStemmer sometimes has issues, so we use a try catch block)
-    try stemmedStr = porterStemmer(strtrim(rawWords(i)));
+    try stemmedStr = porterStemmer(char(strtrim(rawStr)));
     catch stemmedStr = ''; continue;
     end
 
